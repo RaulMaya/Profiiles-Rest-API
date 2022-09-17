8046cdaf33231ebe8ebac78d2c4e80bb4234f024
Raul <raulmayas20@gmail.com>
2022-09-17 12:23:03 -0500

    Managing permissions so users can only edit their own profile

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,9 +1,9 @@
-from email import message
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, viewsets
+from rest_framework.authentication import TokenAuthentication
 
-from profiles_api import serializers, models
+from profiles_api import serializers, models, permissions
 
 
 
@@ -102,4 +102,6 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     """Creates and Updates an User Profile"""
     serializer_class = serializers.UserProfileSerializer
     queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
      
