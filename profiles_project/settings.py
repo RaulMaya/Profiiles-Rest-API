1756fe64f3afde5256a0211c6b7059134892d0c9
Raul <raulmayas20@gmail.com>
2022-09-18 19:59:17 -0500

    Set up for deployment

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 from re import S
-from .config import key
+# from .config import key
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = key
+SECRET_KEY = 'django-insecure-d(g85-q1xnt5w+*yib*(^vsykd1cc!+_3-9dz63qrc21x)^wfm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.environ.get('DEBUG', 1)))
